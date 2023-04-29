952e58f762c281b886e01852a9d766162ba3dc28
Agustina <agusgl98@gmail.com>
2023-04-29 00:54:29 -0300

    Continuación de estructura para Login

---
+++ src/main/java/com/primerproyecto/mgb4/Security/Service/RolService.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.primerproyecto.mgb4.Security.Service;
+
+import com.primerproyecto.mgb4.Security.Entity.Rol;
+import com.primerproyecto.mgb4.Security.Enums.RolNombre;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.primerproyecto.mgb4.Security.Repository.iRolRepository;
+
+@Service
+@Transactional //persistencia en la base de datos. si una operacion falla
+//no se demuestra al cliente
+public class RolService {
+    @Autowired
+    iRolRepository irolRepository;
+    
+    public Optional<Rol> getByRolNombre(RolNombre rolNombre){
+       return irolRepository.findByRolNombre(rolNombre);  
+    }
+   
+    public void save(Rol rol){
+        irolRepository.save(rol);
+    }
+}
+
